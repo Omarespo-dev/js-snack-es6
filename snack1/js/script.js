31bf09ed82357c9c6abc7ea97e2435433107a7c0
Omarespo-dev <omaresposito21@gmail.com>
2025-01-16 14:13:13 +0100

    first commit

---
+++ snack1/js/script.js
@@ -0,0 +1,30 @@
+// Snack 1
+// Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
+
+
+
+
+// Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+const biciCorsa = [
+    {"nome": "Bici1","peso": 30},
+    {"nome": "Bici2","peso": 20},
+    {"nome": "Bici3","peso": 25},
+    {"nome": "Bici4","peso": 26},
+    {"nome": "Bici5","peso": 35},
+    {"nome": "Bici6","peso": 32},
+    {"nome": "Bici7","peso": 38},
+    {"nome": "Bici8","peso": 19},
+]
+
+// Stampare a schermo la bici con peso minore.
+
+// Utilizzo un ciclo for dove gli dico ciclami dentro a bicicorsa finche cnon trovi "il peso minore "
+for(let i = 0; i < biciCorsa.length; i++){
+    let biciIesima= biciCorsa[i]
+    // console.log(biciIesima)
+
+    let biciPeso=biciIesima.peso
+    console.log(biciPeso)
+    // prendimi il numero minore dall array
+    
+}                
